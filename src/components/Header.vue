c25c3b3d4c185cbbdc8743d2d969f3517be70888
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-01-28 07:53:26 +0100

    Inserito logo + creata Header Data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,68 @@
 <template>
   <header>
-    
+    <div class="container">
+      <div class="logo">
+        <img src="../assets/img/dc-logo.png" alt="DC">
+      </div>
+      <nav>
+        <ul>
+          <li>a</li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  data() {
+    return {
+      menuItems: [
+        {
+          text: "Characters",
+          url: "#"
+        },
+        {
+          text: "Comics",
+          url: "#"
+        },
+        {
+          text: "Movies",
+          url: "#"
+        },
+        {
+          text: "TV",
+          url: "#"
+        },
+        {
+          text: "Games",
+          url: "#"
+        },
+        {
+          text: "Collectibles",
+          url: "#"
+        },
+        {
+          text: "Videos",
+          url: "#"
+        },
+        {
+          text: "Fans",
+          url: "#"
+        },
+        {
+          text: "News",
+          url: "#"
+        },
+        {
+          text: "Shop",
+          url: "#"
+        }
+      ],
+      currentItem: 0
+    }
+  }
 }
 </script>
 
