75a98d484e0c27a18e7e0f4c8931e83398c997e5
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-01-31 00:47:11 +0100

    Finito style footer top

--- src/components/FooterTop.vue
+++ src/components/FooterTop.vue
@@ -1,9 +1,14 @@
 <template>
   <section>
-    <div v-for="(item, i) in footerTopItems" :key="i" class="footer-top-item">
-      <a :href="item.url"><img :src="require('../assets/img/' + item.img)" alt=""></a>
-      <div class="footer-top-item-name">{{item.text}}</div>
+    <div class="container">
+      <div v-for="(item, i) in footerTopItems" :key="i" class="footer-top-item">
+        <a :href="item.url">
+          <img :src="require('../assets/img/' + item.img)" alt="">
+          <span class="footer-top-item-name">{{item.text}}</span>
+        </a>
+      </div>
     </div>
+    
   </section>
 </template>
 
@@ -49,6 +54,44 @@ export default {
 @import "../assets/style/partials/variables.scss";
 
 section {
-  background-color: $mainColor;
+    background-color: $mainColor;
+}
+
+.container {
+  height: 150px;
+
+  font-family: 'Bangers', cursive;
+
+  display: flex;
+  justify-content: space-around;
+}
+
+.footer-top-item {
+  width: 236px;
+  height: 55px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    color: white;
+    
+    display: flex;
+    align-items: center;
+
+
+    img {
+      height: 55px;
+      margin-right: 10px;
+
+    }
+  }
+  
+  &:last-child {
+    img {
+      height: 35px;
+    }
+  }
 }
 </style>
