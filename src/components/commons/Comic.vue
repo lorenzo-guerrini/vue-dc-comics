b395da055bb847d65dc89ab6520e41ccf88e0e19
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-01-31 09:36:17 +0100

    Comics e comic per i fumetti

---
+++ src/components/commons/Comic.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="comic">
+      <a href="#">
+          <div class="comic-thumb">
+            <img :src="comic.thumb" :alt="comic-series">
+          </div>
+          <div class="comic-series">
+          {{comic.series}}
+          </div>
+      </a>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Comic',
+    props: {
+        'comic': Object
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/style/global.scss";
+@import "../../assets/style/partials/variables.scss";
+
+.comic {
+    height: 262px;
+    width: 210px;
+
+    padding: 0 12px;
+
+    a {
+        color: white;
+    }
+
+    .comic-thumb {
+        height: 175px;
+
+        text-align: center;
+
+        overflow: hidden;
+
+        img {
+            width: 175px;
+        }
+    }
+
+    .comic-series {
+        margin: 15px 5.5px;
+    }
+}
+</style>
---
+++ src/components/sections/Comics.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="series-container">
+        <Comic v-for="(element, i) in comics" :key="i" :comic="element" />
+    </div>
+</template>
+
+<script>
+import Comic from '../commons/Comic.vue'
+import comicsData from '../../data/dc-comics.json'
+
+export default {
+    name: 'Comics',
+    components: {
+        Comic
+    },
+    data() {
+        return {
+            comics: comicsData
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/style/global.scss";
+@import "../../assets/style/partials/variables.scss";
+
+.series-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+</style>
