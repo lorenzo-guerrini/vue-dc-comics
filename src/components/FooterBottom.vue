0c3f2f72b9d8766127e1d748776728aa4b0be360
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-01-31 06:04:24 +0100

    fine footer bottom

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -1,20 +1,22 @@
 <template>
   <section>
-    <!-- sx -->
-    <div>
-      <button>Sign-up now!</button>
-    </div>
+    <div class="container">
+      <!-- sx -->
+      <div>
+        <button>Sign-up now!</button>
+      </div>
 
-    <!-- dx -->
-    <div>
-      <span>Follow us</span>
-      <ul>
-        <li v-for="(social, i) in socials" :key="i">
-          <a :href="social.url">
-            <img :src="require('../assets/img/' + social.icon)" :alt="social.name">
-          </a>
-        </li>
-      </ul>
+      <!-- dx -->
+      <div id="footer-bottom-dx">
+        <span>Follow us</span>
+        <ul>
+          <li v-for="(social, i) in socials" :key="i">
+            <a :href="social.url">
+              <img :src="require('../assets/img/' + social.icon)" :alt="social.name">
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
@@ -59,4 +61,51 @@ export default {
 <style scoped lang="scss">
 @import "../assets/style/global.scss";
 @import "../assets/style/partials/variables.scss";
+
+section {
+  background-color: $tertiaryColor;
+}
+
+.container {
+  padding: 30px 14px;
+
+  justify-content: space-between;
+
+  font-family: 'Bangers', cursive;
+}
+
+button {
+  font-family: 'Bangers', cursive;
+  font-size: 20px;
+  color: white;
+
+  padding: 12px;
+
+  background: none;
+
+  border: 2px solid $mainColor;
+
+  cursor: pointer;
+}
+
+#footer-bottom-dx {
+  display: flex;
+  align-items: center;
+
+  span {
+    font-size: 26px;
+    padding-right: 10px;
+
+    color: $mainColor;
+  }
+
+  ul {
+    display: flex;
+
+    li {
+      margin-left: 15px;
+    }
+  }
+}
+
 </style>
